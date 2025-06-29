8f2e088dc9fa44b603718cbe86e1aae5ffe87968
NammanShukla <nammanshukla262@gmail.com>
2025-06-29 23:01:17 +0530

    fix: correct casing of 'window' in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { Buffer } from 'buffer';
-Window.Buffer = Buffer; 
+window.Buffer = Buffer; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
